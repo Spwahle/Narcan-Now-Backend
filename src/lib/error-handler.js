c80f3652c816236ae8d1a4e3d3066bed588d0985
Shaun Wahle <shaun.wahle@gmail.com>
2017-10-20 18:30:14 -0700

    Adds bacic-backend scaffold

---
+++ src/lib/error-handler.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const createError = require('http-errors');
+const debug = require('debug')('parkify:error-handler');
+
+module.exports = function(error, request, response, next) {
+  debug('Error handler.');
+
+  if (error.name === 'ValidationError') {
+    error = createError(400, error.message);
+  }
+
+  if (!error.status) {
+    error = createError(500, error.message);
+  }
+
+  debug('Error name: ' + error.name);
+  debug('Error message: ' + error.message);
+
+  response.status(error.status).send(error.name);
+  next();
+};
